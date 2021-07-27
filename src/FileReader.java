023997ced3a773f05040566483702dacc45614ba
zahra <z.khotanlou@gmail.com>
2021-07-27 10:39:36 +0430

    better variable name

--- src/FileReader.java
+++ src/FileReader.java
@@ -7,13 +7,13 @@ import java.util.Map;
 public class FileReader {
     public Map<String, String> readDocument(String path) {
         final String Back_SLASH = "\\";
-        final String PATH = path + Back_SLASH;
+        final String documentsAddress = path + Back_SLASH;
         Map<String, String> documents = new HashMap<>();
         try {
             File sourceFolder = new File(path);
             for (File sourceFile : sourceFolder.listFiles()) {
                 String fileName = sourceFile.getName();
-                documents.put(fileName, Files.readString(Path.of(PATH , fileName)).
+                documents.put(fileName, Files.readString(Path.of(documentsAddress , fileName)).
                         toLowerCase());//open file
             }
         } catch (Exception e) {
