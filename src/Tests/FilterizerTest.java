5e3e55d42e4506736073bfaef55bbaedf5ad40ab
zahra <z.khotanlou@gmail.com>
2021-07-29 21:25:50 +0430

    Mock objects in FilterizerTest was debugged

--- src/Tests/FilterizerTest.java
+++ src/Tests/FilterizerTest.java
@@ -1,16 +1,17 @@
 package Tests;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 import Phase2.*;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class FilterizerTest {
@@ -24,39 +25,40 @@ public class FilterizerTest {
     @Mock
     WithoutSignFilter withOutSignFilter;
 
-    Set<String> documentsName=new HashSet<>();
-
-    Filterizer filterizer=new Filterizer(plusFilter,minusFilter,withOutSignFilter,documentsName);
-
     @Mock
     QueryKeeper queryKeeper;
 
+    Filterizer filterizer;
+    @Before
+    public void setup(){
+        Set<String> documentsName = new HashSet<>();
+        this.filterizer = new Filterizer(plusFilter,minusFilter,withOutSignFilter,documentsName);
+    }
     @Test
     public void testFilter() {
 
-        Set<String> plusFiltered = new HashSet<>();
-        plusFiltered.add("plus1");
-        plusFiltered.add("plus2");
-        plusFiltered.add("plus3");
+        Set<String> plusQueryCategory = new HashSet<>();
+        plusQueryCategory.add("plus1");
+        plusQueryCategory.add("plus2");
+        plusQueryCategory.add("plus3");
 
-        Set<String> withoutSignFiltered = new HashSet<>();
-        withoutSignFiltered.add("none1");
-        withoutSignFiltered.add("none2");
-        withoutSignFiltered.add("none3");
+        Set<String> withoutSignQueryCategory = new HashSet<>();
+        withoutSignQueryCategory.add("none1");
+        withoutSignQueryCategory.add("none2");
+        withoutSignQueryCategory.add("none3");
 
-        Set<String> minusFiltered = new HashSet<>();
-        minusFiltered.add("plus1");
-        minusFiltered.add("minus2");
-        minusFiltered.add("minus3");
+        Set<String> minusQueryCategory = new HashSet<>();
+        minusQueryCategory.add("plus1");
+        minusQueryCategory.add("minus2");
+        minusQueryCategory.add("minus3");
 
-        Mockito.when(queryKeeper.getMinusContain()).thenReturn(minusFiltered);
-        Mockito.when(queryKeeper.getPlusContain()).thenReturn(plusFiltered);
-        Mockito.when(queryKeeper.getWithOutSign()).thenReturn(withoutSignFiltered);
+        Mockito.when(queryKeeper.getMinusContain()).thenReturn(minusQueryCategory);
+        Mockito.when(queryKeeper.getPlusContain()).thenReturn(plusQueryCategory);
+        Mockito.when(queryKeeper.getWithOutSign()).thenReturn(withoutSignQueryCategory);
 
         String testedFiltered = "plus1";
 
-        Set<String> resultFiltered=new HashSet<>();
-        resultFiltered=filterizer.filter(queryKeeper);
+        Set<String> resultFiltered = filterizer.filter(queryKeeper);
 
         Assert.assertTrue(resultFiltered.contains(testedFiltered));
 
