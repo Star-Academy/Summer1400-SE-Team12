6ef5fb5f39797c9e6fe24e662c7c3edaf4ee05cb
zahra <z.khotanlou@gmail.com>
2021-07-29 20:32:25 +0430

    test mockito notNull

--- src/Tests/FilterizerTest.java
+++ src/Tests/FilterizerTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.mockito.Mockito.when;
 
