175f75f8439104d09a58223e0cec700724f87745
zahra <z.khotanlou@gmail.com>
2021-07-30 14:08:38 +0430

    delete fileReader property from InvertedIndex

--- src/InvertedIndex.java
+++ src/InvertedIndex.java
@@ -3,36 +3,29 @@ import java.util.*;
 public class InvertedIndex {
 
     private final HashMap<String, Set<String>> invertedIndex = new HashMap<>();
-    private final FileReader fileReader;
 
-    public InvertedIndex(FileReader fileReader) {
-        this.fileReader = fileReader;
-    }
-
-    public void buildInvertedIndex(Map<String, String[]> splittedDocumentInfo) {
-        for (Map.Entry<String, String[]> doc : splittedDocumentInfo.entrySet()) {
-            for (int j = 0; j < doc.getValue().length; j++) {
-                if (invertedIndex.containsKey(doc.getValue()[j]))
-                    invertedIndex.get(doc.getValue()[j]).add(doc.getKey());
-                else {
-                    Set<String> documentNames = new HashSet<>();
-                    documentNames.add(doc.getKey());
-                    invertedIndex.put(doc.getValue()[j], documentNames);
-                }
-            }
+    public void buildInvertedIndex(Map<String, String> DocNameMapToContent) {
+        for (Map.Entry<String, String> doc : DocNameMapToContent.entrySet()) {
+            String[] words = splitDocumentsWords(doc.getValue());
+            addDocumentWords(doc.getKey(), words);
         }
     }
 
-    public Map<String, String[]> splitDocumentsWords(String pathOfTheDocument) {
-        Map<String, String[]> splittedDocumentInfo = new HashMap<>();
-        Map<String, String> document = fileReader.readDocuments(pathOfTheDocument);
-        for (Map.Entry<String, String> doc : document.entrySet())
-            splittedDocumentInfo.put(doc.getKey(), doc.getValue().split("[\\W]+"));
-        return splittedDocumentInfo;
+    private String[] splitDocumentsWords(String docContent) {
+        return docContent.split("[\\W]+");
+    }
+
+    private void addDocumentWords(String docName, String[] docWords) {
+        for (String word : docWords) {
+            if(invertedIndex.containsKey(word))
+                invertedIndex.get(word).add(docName);
+            else
+                invertedIndex.put(word, new HashSet<>(Arrays.asList(docName)));
+        }
     }
 
     public Set<String> getInvertedIndexValue(String key) {
-        return invertedIndex.getOrDefault(key,new HashSet<>());
+        return invertedIndex.getOrDefault(key, new HashSet<>());
     }
 
 }
