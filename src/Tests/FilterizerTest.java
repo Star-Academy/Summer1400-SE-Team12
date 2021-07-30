91278b74d1457b4f424155abb2df015003a430f2
zahra <z.khotanlou@gmail.com>
2021-07-31 00:30:37 +0430

    changes on FilterizerTest

--- src/Tests/FilterizerTest.java
+++ src/Tests/FilterizerTest.java
@@ -1,92 +1,92 @@
 package Tests;
+
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-import Phase2.*;
-import org.junit.Assert;
+import Phase02.*;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class FilterizerTest {
 
     @Mock
-    PlusFilter plusFilter;
-
+    PlusFilter mockedPlusFilter;
     @Mock
-    MinusFilter minusFilter;
-
+    MinusFilter mockedMinusFilter;
     @Mock
-    WithoutSignFilter withOutSignFilter;
-
-    Set<String> documentsName=new HashSet<>();
-
-    Filterizer filterizer=new Filterizer(plusFilter,minusFilter,withOutSignFilter,documentsName);
-
+    WithoutSignFilter mockedWithOutSignFilter;
     @Mock
-    QueryKeeper queryKeeper;
+    InvertedIndex mockedInvertedIndex;
+    @Mock
+    QueryKeeper mockedQueryKeeper;
 
+    private Set<String> documentsName;
 
-    @Test
-    public void testFilter() {
-
-        Set<String> nullSet = new HashSet<>();
-
-        Set<String> plusFiltered = new HashSet<>();
-        plusFiltered.add("plus1");
-        plusFiltered.add("plus2");
-        plusFiltered.add("plus3");
-
-        Set<String> withoutSignFiltered = new HashSet<>();
-        withoutSignFiltered.add("none1");
-        withoutSignFiltered.add("none2");
-        withoutSignFiltered.add("none3");
-
-        Set<String> minusFiltered = new HashSet<>();
-        minusFiltered.add("plus1");
-        minusFiltered.add("minus2");
-        minusFiltered.add("minus3");
-
-        Set<String> plusDocsName = new HashSet<>();
-        plusDocsName.add("t1");
-        plusDocsName.add("t2");
-        plusDocsName.add("t3");
-
-        Set<String> minusDocsName = new HashSet<>();
-        plusDocsName.add("t4");
-        plusDocsName.add("t5");
-        plusDocsName.add("t6");
-
-        Set<String> noneDocsName = new HashSet<>();
-        plusDocsName.add("t1");
-        plusDocsName.add("t7");
-        plusDocsName.add("t8");
-
-        Mockito.when(queryKeeper.getMinusContain()).thenReturn(minusFiltered);
-        Mockito.when(queryKeeper.getPlusContain()).thenReturn(plusFiltered);
-        Mockito.when(queryKeeper.getWithOutSign()).thenReturn(withoutSignFiltered);
-
-        Mockito.when(plusFilter.filter(plusFiltered,nullSet)).thenReturn(plusDocsName);
-        Mockito.when(minusFilter.filter(minusFiltered,nullSet)).thenReturn(minusDocsName);
-        Mockito.when(withOutSignFilter.filter(withoutSignFiltered,nullSet)).thenReturn(noneDocsName);
-
-        String testedFiltered = "t1";
-        //کلمه مشترک بین شان Todo
-
-        Set<String> resultFiltered;
-        resultFiltered=filterizer.filter(queryKeeper);
-
-        Assert.assertTrue(resultFiltered.contains(testedFiltered));
-
+    @Before
+    public void setup() {
+        this.documentsName = new HashSet<>(Arrays.asList("t1","t2","t3","t4","t5", "t6",
+                "t7", "t8","t9","t10","t11","t12"));
+        Filterizer filterizer = new Filterizer(mockedPlusFilter, mockedMinusFilter, mockedWithOutSignFilter,
+                mockedInvertedIndex, documentsName);
     }
 
 
+    @Test
+    public void testFilterizerWithAllTypesOfFilter() {
+        Set<String> plusDocsName = new HashSet<>(Arrays.asList("t1","t2","t3"));
+        Set<String> minusDocsName = new HashSet<>(Arrays.asList("t4","t5","t6"));
+        Set<String> noneDocsName = new HashSet<>(Arrays.asList("t1","t7","t8"));
 
+        Mockito.when(mockedQueryKeeper.getPlusContain()).thenReturn(plusDocsName);
+        Mockito.when(mockedQueryKeeper.getMinusContain()).thenReturn(minusDocsName);
+        Mockito.when(mockedQueryKeeper.getWithOutSign()).thenReturn(noneDocsName);
+
+        Mockito.doAnswer(invocation -> {
+            
+        }).when(mockedPlusFilter).filter(Mockito.anySet(),Mockito.anySet());
+
+//        Set<String> nullSet = new HashSet<>();
+//
+//        Set<String> plusFiltered = new HashSet<>();
+//        plusFiltered.add("plus1");
+//        plusFiltered.add("plus2");
+//        plusFiltered.add("plus3");
+//
+//        Set<String> withoutSignFiltered = new HashSet<>();
+//        withoutSignFiltered.add("none1");
+//        withoutSignFiltered.add("none2");
+//        withoutSignFiltered.add("none3");
+//
+//        Set<String> minusFiltered = new HashSet<>();
+//        minusFiltered.add("plus1");
+//        minusFiltered.add("minus2");
+//        minusFiltered.add("minus3");
+//
+
+//
+
+//
+//        Mockito.when(plusFilter.filter(plusFiltered, nullSet)).thenReturn(plusDocsName);
+//        Mockito.when(minusFilter.filter(minusFiltered, nullSet)).thenReturn(minusDocsName);
+//        Mockito.when(withOutSignFilter.filter(withoutSignFiltered, nullSet)).thenReturn(noneDocsName);
+//
+//        String testedFiltered = "t1";
+//        //کلمه مشترک بین شان Todo
+//
+//        Set<String> resultFiltered;
+//        resultFiltered = filterizer.filter(queryKeeper);
+//
+//        Assert.assertTrue(resultFiltered.contains(testedFiltered));
+
+    }
 
 
 }
