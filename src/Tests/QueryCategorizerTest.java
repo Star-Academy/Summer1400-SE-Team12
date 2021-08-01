5869b3c53d4b3eaa0f0c3cb2d2772dd65c8263f2
zahra <z.khotanlou@gmail.com>
2021-08-01 10:39:08 +0430

    QueryCategorizerTest was completed

--- src/Tests/QueryCategorizerTest.java
+++ src/Tests/QueryCategorizerTest.java
@@ -1,33 +1,46 @@
-//package Tests;
-//
-//
-//import Phase02.MinusFilter;
-//import Phase02.QueryCategorizer;
-//import org.junit.Assert;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.mockito.Mock;
-//
-//import java.io.ByteArrayInputStream;
-//
-//public class QueryCategorizerTest {
-//
-//    private QueryCategorizer queryCategorizer;
-//
-//    @Before
-//    public void setup(){
-//        this.queryCategorizer = new QueryCategorizer();
-//    }
-//
-//    @Test
-//    public void testCategorizeQuery() {
-//
-//        String query = "mom dad +sister +sisi -brother -bro";
-//        System.setIn(new ByteArrayInputStream(query.getBytes()));
-//
-//        Assert.asser
-//
-//    }
-//
-//
-//}
+package Tests;
+
+import Phase02.QueryCategorizer;
+import org.junit.*;
+
+import java.io.ByteArrayInputStream;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class QueryCategorizerTest {
+
+    private QueryCategorizer queryCategorizer;
+
+    @Before
+    public void setup(){
+        this.queryCategorizer = new QueryCategorizer();
+        String query = "mom dad +sister +sisi -brother -bro";
+        System.setIn(new ByteArrayInputStream(query.getBytes()));
+    }
+
+    @Test
+    public void testCategorizeForNoSignQueries() {
+        queryCategorizer.categorizeQuery();
+        Assert.assertArrayEquals(new String[]{"dad","mom"},
+                queryCategorizer.getQueryKeeper().getWithOutSign().toArray());
+
+    }
+
+    @Test
+    public void testCategorizeForPlusQueries() {
+        queryCategorizer.categorizeQuery();
+        Assert.assertArrayEquals(new String[]{"sisi","sister"},
+                queryCategorizer.getQueryKeeper().getPlusContain().toArray());
+
+    }
+
+    @Test
+    public void testCategorizeForMinusQueries() {
+        queryCategorizer.categorizeQuery();
+        Assert.assertArrayEquals(new String[]{"brother","bro"},
+                queryCategorizer.getQueryKeeper().getMinusContain().toArray());
+
+    }
+
+
+}
