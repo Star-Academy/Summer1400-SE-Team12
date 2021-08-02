41e9fc833c8e46075f5288f60dc3de63ff37e757
zahra <z.khotanlou@gmail.com>
2021-08-02 20:02:46 +0430

    variable name changed

--- Phase04/Phase04/Phase04/Program.cs
+++ Phase04/Phase04/Phase04/Program.cs
@@ -2,16 +2,16 @@
 {
     class Program
     {
-        private static string _pathNames = @"JsonFiles\Students.json";
-        private static string _pathPoints = @"JsonFiles\Scores.json";
+        private static string _pathStudents = @"JsonFiles\Students.json";
+        private static string _pathScores = @"JsonFiles\Scores.json";
         
         static void Main(string[] args)
         {
             var reader = new Reader();
             var information = new AverageEngine();
             
-            var students = reader.ReadJson<Student>(_pathNames);
-            var studentsScores = reader.ReadJson<StudentScore>(_pathPoints);
+            var students = reader.ReadJson<Student>(_pathStudents);
+            var studentsScores = reader.ReadJson<StudentScore>(_pathScores);
             var studentsAverage = information.calculateAvg(students, studentsScores);
             information.PrintTopStudents(3,studentsAverage);
 
