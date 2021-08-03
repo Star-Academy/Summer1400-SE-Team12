094ca9a766e8ae5ad497b63da851f9d52563c45c
Neda Taghizadeh <59171120+nedataghizadeh79@users.noreply.github.com>
2021-08-03 12:33:56 +0430

    change first.

--- Phase05/Phase05/Phase05/Program.cs
+++ Phase05/Phase05/Phase05/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Threading.Channels;
 
 namespace Phase05
 {
     class Program
     {
-        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Neda changed");
+        }
     }
 }
