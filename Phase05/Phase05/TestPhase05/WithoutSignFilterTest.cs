f76aab46a770371069b8cc0e07662707a82f32f8
zahra <z.khotanlou@gmail.com>
2021-08-04 12:37:20 +0430

    error fixed

--- Phase05/Phase05/TestPhase05/WithoutSignFilterTest.cs
+++ Phase05/Phase05/TestPhase05/WithoutSignFilterTest.cs
@@ -1,7 +1,42 @@
-﻿namespace DefaultNamespace
+﻿using System.Collections.Generic;
+using NSubstitute;
+using Phase05;
+using Xunit;
+
+namespace TestPhase05
 {
     public class WithoutSignFilterTest
     {
+        private readonly WithoutSignFilter withoutSignFilter;
+        private readonly IInvertedIndex _invertedIndex;
+
+        public WithoutSignFilterTest()
+        {
+            _invertedIndex = Substitute.For<IInvertedIndex>();
+            withoutSignFilter= new WithoutSignFilter(_invertedIndex);
+        }
+
+
+        [Fact]
+        public void FilterTest_shouldReturnDocumentsThatContainSingleQuery()
+        {
+            
+            var expected = new HashSet<string>() {"s3"};
+            var withoutSignFilterQuery = new HashSet<string>() {"hello", "bye"};
+            SetupInvertedIndexGetMethod();
+            var actual = withoutSignFilter.Filter(withoutSignFilterQuery);
+            Assert.Equal(expected, actual);
+            
+        }
+
         
+        private void SetupInvertedIndexGetMethod()
+        {
+            _invertedIndex.GetInvertedIndexValue("hello").Returns(new HashSet<string>() {"s1", "s2", "s3"});
+            _invertedIndex.GetInvertedIndexValue("bye").Returns(new HashSet<string>() {"s3", "s4", "s5"});
+        }
+
+
+
     }
 }
