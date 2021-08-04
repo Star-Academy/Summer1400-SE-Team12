507368b4f975d346ec0bc438cd74a6133d7c1246
zahra <z.khotanlou@gmail.com>
2021-08-04 12:31:33 +0430

    tests was added

--- Phase05/Phase05/TestPhase05/MinusFilterTest.cs
+++ Phase05/Phase05/TestPhase05/MinusFilterTest.cs
@@ -1,35 +1,7 @@
-﻿using System.Collections.Generic;
-using NSubstitute;
-using Phase05;
-using Xunit;
-
-namespace TestPhase05
+﻿namespace DefaultNamespace
 {
     public class MinusFilterTest
     {
-        private readonly MinusFilter _minusFilterSUT;
-        private readonly IInvertedIndex _invertedIndex;
-
-        public MinusFilterTest()
-        {
-            _invertedIndex = Substitute.For<IInvertedIndex>();
-            _minusFilterSUT = new MinusFilter(_invertedIndex);
-        }
-
-        [Fact]
-        public void MinusFilter_shouldReturnRemovedDocumentsContainSingleQuery()
-        {
-            var expected = new HashSet<string>() {"doc1", "doc2", "doc3", "doc4", "doc5"};
-            var minusQuery = new HashSet<string>() {"hello", "bye"};
-            SetupInvertedIndexGetMethod();
-            var actual = _minusFilterSUT.Filter(minusQuery);
-            Assert.Equal(expected, actual);
-        }
-
-        private void SetupInvertedIndexGetMethod()
-        {
-            _invertedIndex.GetInvertedIndexValue("hello").Returns(new HashSet<string>() {"doc1", "doc2", "doc3"});
-            _invertedIndex.GetInvertedIndexValue("bye").Returns(new HashSet<string>() {"doc3", "doc4", "doc5"});
-        }
+        
     }
 }
