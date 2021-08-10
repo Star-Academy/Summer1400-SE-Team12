c8d429554a74be54e483a6e01984c63a48fe4cf1
zahra <z.khotanlou@gmail.com>
2021-08-10 11:08:46 +0430

    TestPhase08 added.

---
+++ Phase08/Phase08/TestPhase08/DisjunctionFilterTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using NSubstitute;
+using Phase05;
+using Xunit;
+
+namespace TestPhase05
+{
+    public class DisjunctionFilterTest
+    {
+        private readonly DisjunctionFilter _disjunctionFilter;
+        private readonly IInvertedIndex _invertedIndex;
+
+        public DisjunctionFilterTest()
+        {
+            _invertedIndex = Substitute.For<IInvertedIndex>();
+            _disjunctionFilter = new DisjunctionFilter(_invertedIndex);
+        }
+
+        [Fact]
+        public void MinusFilterTest_shouldReturnRemovedDocumentsContainSingleQuery()
+        {
+            var expected = new HashSet<string>() {"doc1", "doc2", "doc3", "doc4", "doc5"};
+            var minusQuery = new HashSet<string>() {"hello", "bye"};
+            SetupInvertedIndexGetMethod();
+            var actual = _disjunctionFilter.Filter(minusQuery);
+            Assert.Equal(expected, actual);
+        }
+
+        private void SetupInvertedIndexGetMethod()
+        {
+            _invertedIndex.GetInvertedIndexValue("hello").Returns(new HashSet<string>() {"doc1", "doc2", "doc3"});
+            _invertedIndex.GetInvertedIndexValue("bye").Returns(new HashSet<string>() {"doc3", "doc4", "doc5"});
+        }
+    }
+}
