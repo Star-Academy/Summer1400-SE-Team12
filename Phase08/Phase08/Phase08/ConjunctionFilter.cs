247f127b927e388f2357237d066871917e1959a9
zahra <z.khotanlou@gmail.com>
2021-08-14 17:46:19 +0430

    tests added

--- Phase08/Phase08/Phase08/ConjunctionFilter.cs
+++ Phase08/Phase08/Phase08/ConjunctionFilter.cs
@@ -7,23 +7,23 @@ namespace Phase08
 {
     public class ConjunctionFilter : IFilter
     {
-        private readonly DbSet<Word> _wordDBSet;
+        private readonly DbSet<Word> _wordDbSet;
 
         public ConjunctionFilter(DbSet<Word> wordDbSet)
         {
-            _wordDBSet = wordDbSet;
+            _wordDbSet = wordDbSet;
         }
 
         public ISet<string> Filter(ISet<string> signQueries)
         {
             var firstQuery = signQueries.First();
             ISet<string> conjunctionFiltered = new HashSet<string>(
-                _wordDBSet.Find(firstQuery)?.
+                _wordDbSet.Find(firstQuery)?.
                     DocsCollection.
                     Select(doc => doc.DocName) ?? new HashSet<string>());
 
             return signQueries.Aggregate(conjunctionFiltered, (current, query) =>
-                current.Intersect(_wordDBSet.Find(query)?.
+                current.Intersect(_wordDbSet.Find(query)?.
                                       DocsCollection.
                                       Select(doc => doc.DocName) ?? new HashSet<string>()).
                     ToHashSet());
---
+++ Phase08/Phase08/TestEFCorePhase08/QueryCategorizerTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Phase08;
+using Xunit;
+
+namespace TestEFCorePhase08
+{
+    public class QueryCategorizerTest
+    {
+        private readonly IQueryCategorizer _queryCategorizer;
+
+        public QueryCategorizerTest()
+        {
+            _queryCategorizer = new QueryCategorizer();
+        }
+        
+        [Fact]
+        public void TestCategorize_ForNoSignQueries() {
+            string[] queries = {"mom","dad","+sister","+sisi","-brother","-bro"};
+            var categorized = _queryCategorizer.CategorizeQueries(queries);
+            
+            Assert.Equal(new HashSet<string>() {"sisi", "sister"},categorized.GetPlusContain());
+            Assert.Equal(new HashSet<string>() {"bro", "brother"},categorized.GetMinusContain());
+            Assert.Equal(new HashSet<string>() {"mom", "dad"},categorized.GetWithoutSignContain());
+
+        }
+        
+    }
+}
