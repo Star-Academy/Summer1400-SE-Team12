0294145db14039d77e58b4f4987e7c1b58a7708f
zahra <z.khotanlou@gmail.com>
2021-08-15 10:30:25 +0430

    tests added

--- Phase08/Phase08/TestEFCorePhase08/InvertedIndexTest.cs
+++ Phase08/Phase08/TestEFCorePhase08/InvertedIndexTest.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 using Phase08;
+using SQLHandler;
 using Xunit;
 
 namespace TestEFCorePhase08
 {
     public class InvertedIndexTest
     {
-        private readonly InvertedIndex _invertedIndex;
+        private readonly DbSet<Word> _wordDbSet;
         private readonly Dictionary<string,string> _docNameMapToContent;
 
 
         public InvertedIndexTest()
         {
-            _invertedIndex = new InvertedIndex();
+            
             _docNameMapToContent = new Dictionary<string, string>
             {
                 {"text1", "one two"}, {"text2", "five six seven eight nine"}, {"text3", "one two three "}
@@ -22,9 +24,30 @@ namespace TestEFCorePhase08
 
         [Fact]
         public void TestBuildInvertedIndex_WordExistInADoc() {
-            _invertedIndex.BuildInvertedIndex(_docNameMapToContent);
-            Assert.Equal(new HashSet<string>(){"text2"}, 
-                _invertedIndex.GetInvertedIndexValue("six"));
+            var options = new DbContextOptionsBuilder<InvertedIndexContext>()
+                .UseInMemoryDatabase(databaseName: "EfcorePhase08Project")
+                .Options;
+
+            // Insert seed data into the database using one instance of the context
+            using (var context = new InvertedIndexContext())
+            {
+                context.WordsDbContext.Add(new Word());
+                context.Movies.Add(new Movie {Id = 2, Title = "Movie 2", YearOfRelease = 2018, Genre = "Action"});
+                context.Movies.Add(nnew Movie {Id = 3, Title = "Movie 3", YearOfRelease = 2019, Genre = "Action"});
+                context.SaveChanges();
+            }
+
+            // Use a clean instance of the context to run the test
+            using (var context = new MovieDbContext(options))
+            {
+                MovieRepository movieRepository = new MovieRepository(context);
+                List<Movies> movies == movieRepository.GetAll()
+
+                Assert.Equal(3, movies.Count);
+            }
+            // _invertedIndex.BuildInvertedIndex(_docNameMapToContent);
+            // Assert.Equal(new HashSet<string>(){"text2"}, 
+            //     _invertedIndex.GetInvertedIndexValue("six"));
 
         }
 
